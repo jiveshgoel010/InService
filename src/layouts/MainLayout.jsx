4b2e5f32f699c95fd4e5bd5593200b1170eb8bfb
tanmaya-panigrahi <tanmayapanigrahi08@gmail.com>
2024-11-05 00:29:26 +0530

    add hiring vendor form from client side

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -14,7 +14,7 @@ const MainLayout = () => {
                 <main >
                     <Outlet />
                 </main>
-                {/* <Footer /> */}
+                <Footer />
 
             </div>
         </>
